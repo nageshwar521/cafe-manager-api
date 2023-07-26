b36b9915fe8a5ef7013e04dca3e1375acf0cf227
Nageshwar Reddy Pandem <nageshwar.uideveloper@gmail.com>
2023-07-27 01:11:54 +0800

    fixed bugs

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,6 +1,6 @@
 const { default: knex } = require("knex");
 const environment = process.env.NODE_ENV;
-const dbConfig = require("../knexfile")[environment];
+const dbConfig = require("../../knexfile")[environment];
 // console.log(dbConfig, "db config");
 
 const knexInstance = knex(dbConfig);

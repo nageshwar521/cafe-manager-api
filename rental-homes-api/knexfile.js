cad0952e6fcabdb5e94896c92e0c694df7d11fbc
Nageshwar Reddy Pandem <nageshwar.uideveloper@gmail.com>
2023-12-03 17:00:12 +0800

    bug fixes

--- rental-homes-api/knexfile.js
+++ rental-homes-api/knexfile.js
@@ -1,6 +1,8 @@
 const path = require('path');
 
-require("dotenv").config({ path: path.resolve(__dirname, '.env.development') });
+if (process.env.NODE_ENV === 'development') {
+  require("dotenv").config({ path: path.resolve(__dirname, '.env.development') });
+}
 const { devConfig, prodConfig } = require("./src/db/config");
 
 module.exports = {

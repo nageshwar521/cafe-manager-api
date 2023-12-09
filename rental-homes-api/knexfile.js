4b86d91cf3e1122e475d648594627ff6e58667b6
Nageshwar Reddy Pandem <nageshwar.uideveloper@gmail.com>
2023-12-10 00:49:36 +0800

    package scripts added

--- rental-homes-api/knexfile.js
+++ rental-homes-api/knexfile.js
@@ -2,6 +2,8 @@ const path = require('path');
 
 if (process.env.NODE_ENV === 'development') {
   require("dotenv").config({ path: path.resolve(__dirname, '.env.development') });
+} else {
+  require("dotenv").config({ path: path.resolve(__dirname, '.env.production') });
 }
 const { devConfig, prodConfig } = require("./src/db/config");
 

727a63c68e99f645e767e6e5df20d89188938d71
Nageshwar Reddy Pandem <nageshwar.uideveloper@gmail.com>
2023-12-16 16:07:07 +0800

    amenity migration update

--- rental-homes-api/src/db/migrations/20220306144632_amenities.js
+++ rental-homes-api/src/db/migrations/20220306144632_amenities.js
@@ -8,8 +8,8 @@ exports.up = function (knex) {
       return knex.schema.createTable("amenities", function (table) {
         table.uuid("id").defaultTo(knex.raw("(UUID())"));
         table.string("amenity_name").unique().notNullable();
-        table.string("amenity_category").notNullable();
-        table.string("amenity_desc").nullable();
+        table.string("amenity_category").nullable();
+        table.string("amenity_description").nullable();
         table.timestamp("created_at").defaultTo(knex.fn.now());
         table.timestamp("updated_at").defaultTo(knex.fn.now());
       });
